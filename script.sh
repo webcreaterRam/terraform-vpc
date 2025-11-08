964feae6350707ed9ee32c44a66a93dc7841ff03
ramkumar <rpal92448@gmail.com>
2025-11-08 11:36:55 +0530

    This is the first commit done in the morning

--- script.sh
+++ script.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 echo "My name is Terraform VPC Module"
-echo "Creating VPC with Terraform..."
-echo "VPC Create Successfully!"
-# I am adding a comment here to test git diff feature.
-echo "Done!"
+echo "It is a new day with nice morning sunshine!"
